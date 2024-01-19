8a4a00965ea5eab965225f2f392f75a0622294c1
JakesMD <71591438+JakesMD@users.noreply.github.com>
2024-01-19 19:14:25 +0100

    ⚡️ Setup Supabase for Flutter

--- app/lib/main_production.dart
+++ app/lib/main_production.dart
@@ -1,6 +1,16 @@
 import 'package:booksinarow/app/app.dart';
 import 'package:booksinarow/bootstrap.dart';
+import 'package:booksinarow/secrets.dart';
+import 'package:bpub/supabase_flutter.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  bootstrap(() => const BApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: BSecrets.productionDatabaseURL,
+    anonKey: BSecrets.productionDatabaseAnonKey,
+  );
+
+  await bootstrap(() => const BApp());
 }
