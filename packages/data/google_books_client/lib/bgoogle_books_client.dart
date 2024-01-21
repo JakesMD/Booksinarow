2808c534e1d283fdac2b0a5133a1f533228d104e
JakesMD <71591438+JakesMD@users.noreply.github.com>
2024-01-21 18:16:21 +0100

    📝 Update documentation

--- packages/data/google_books_client/lib/bgoogle_books_client.dart
+++ packages/data/google_books_client/lib/bgoogle_books_client.dart
@@ -1,8 +1,7 @@
-/// The bgoogle_books_client library provides functionality for interacting with
-/// the Google Books API.
+/// Provides functionality for interacting with the Google Books API.
 library bgoogle_books_client;
 
 export 'src/bgoogle_books_client.dart';
 export 'src/exceptions/_exceptions.dart';
 export 'src/models/_models.dart';
-export 'src/utils/bhttp.dart';
+export 'src/utils/_utils.dart';
